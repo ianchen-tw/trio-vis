99578ccc4fa6f6e31ed1382e04938baaa827f81a
Ian Chen <ianre657@gmail.com>
2021-03-17 23:50:40 +0800

    ✨ log task tree into json files

--- trio_monitor/monitor.py
+++ trio_monitor/monitor.py
@@ -1,49 +1,74 @@
+import json
 from collections import defaultdict
 
-import ipdb
 import trio
 from rich.console import Console
+from trio.lowlevel import Task
+from trio_typing import Nursery
+from pathlib import Path
 
 console = Console()
 
 
-def task_name_colored(task):
-    return f"[yellow]{task.name}[reset]"
+class TaskRegistry:
+    def __init__(self):
+        self.serial_num_from_name = defaultdict(int)
+        self.registered_tasks = defaultdict(defaultdict)
+
+    def get_task_name(self, task: Task):
+        if task not in self.registered_tasks:
+            serial_num = self.serial_num_from_name[task.name]
+            self.serial_num_from_name[task.name] += 1
+            self.registered_tasks[task]["serial_num"] = serial_num
+        else:
+            serial_num = self.registered_tasks[task]["serial_num"]
+        return f"{task.name}-{serial_num}"
+
+
+class TrioStatusLogger:
+    def __init__(self):
+        self.registry = TaskRegistry()
+
+    def get_system_status_json(self):
+        root_task = trio.lowlevel.current_root_task()
+        result = self.task_to_json(root_task)
+        return result
+
+    def nursery_to_json(self, nursery: Nursery):
+        return {
+            "id": id(nursery),
+            "name": "<nursery>",
+            "tasks": [self.task_to_json(child) for child in nursery.child_tasks],
+        }
+
+    def task_to_json(self, task: Task):
+        return {
+            "id": id(task),
+            "name": self.registry.get_task_name(task),
+            "nurseries": [
+                self.nursery_to_json(nursery) for nursery in task.child_nurseries
+            ],
+        }
 
 
 class TaskLoggingMonitor(trio.abc.Instrument):
     def __init__(self):
-        self.tasks = defaultdict(defaultdict)
-        self.id_in_name = defaultdict(int)
+        self.event_id = 0
+        self.logger = TrioStatusLogger()
 
-    def _new_task_by_name(self, task):
-        i = self.id_in_name[task.name]
-        self.id_in_name[task.name] += 1
-        return i
-
-    def before_run(self):
-        print("[Logger] run")
+    def log_status(self, suffix: str = ""):
+        eid = self.event_id
+        self.event_id += 1
+        dic = self.logger.get_system_status_json()
+        actual_suffix = f"-{suffix}" if suffix != "" else ""
+        Path("./status").mkdir(exist_ok=True)
+        with open(f"./status/event-{eid}{actual_suffix}.json", "w") as outfile:
+            json.dump(dic, outfile, indent=4)
 
     def task_spawned(self, task):
         if "trio/_core" not in task.coro.cr_frame.f_code.co_filename:
-            sid = self._new_task_by_name(task)
-            t = self.tasks[task]
-            t["sid"] = sid
-            t["name"] = f"{task.name}-{sid}"
-            ipdb.set_trace()
-            console.print(
-                f"[green]new task spwaned[orange_red1]({sid})[reset]: [yellow]{t['name']}"
-            )
+            self.log_status(suffix="spawned")
 
     def task_exited(self, task):
-        if task in self.tasks:
-            t = self.tasks[task]
-            sid = t["sid"]
-            console.print(
-                f"[red]task exited[orange_red1]({sid})[reset]: [yellow]{t['name']}"
-            )
-
-    def before_task_step(self, task):
-        if task in self.tasks:
-            t = self.tasks[task]
-            console.print(f"[bright_black]task step: {t['name']}")
+        # if "trio/_core" not in task.coro.cr_frame.f_code.co_filename:
+        self.log_status(suffix="exited")
