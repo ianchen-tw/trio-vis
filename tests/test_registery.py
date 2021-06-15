d687759ccd13018d98a801c041d6462ee82b5f59
Ian Chen <ianre657@gmail.com>
2021-06-15 16:07:32 +0800

    new registry with name parsing

---
+++ tests/test_registery.py
@@ -0,0 +1,45 @@
+import pytest
+
+from trio_monitor.registry import *
+from trio_monitor.trio_fake import FakeTrioNursery, FakeTrioTask
+
+
+@pytest.fixture
+def drawer() -> SerialNumberGen:
+    return SerialNumberGen()
+
+
+@pytest.fixture
+def registry() -> SCRegistry:
+    return SCRegistry()
+
+
+def test_drawer(drawer: SerialNumberGen):
+    assert drawer.draw("a") == 0
+    assert drawer.draw("b") == 0
+    assert drawer.draw("a") == 1
+    assert drawer.draw("c") == 0
+    assert drawer.draw("c") == 1
+
+
+def test_parser():
+    assert parseObjectType(FakeTrioTask(name="t1")) == TYPE_TRIO_TASK
+    assert parseObjectType(FakeTrioNursery(name="n1")) == TYPE_TRIO_NURSERY
+
+
+def test_registry_add(registry: SCRegistry):
+    t0 = FakeTrioTask(name="func1")
+    t1 = FakeTrioTask(name="func1")
+    assert "func1-0" == registry.get_name(t0)
+    assert "func1-1" == registry.get_name(t1)
+    assert "func1-0" == registry.get_name(t0)
+
+
+def test_registry_remove(registry: SCRegistry):
+    t0 = FakeTrioTask(name="func1")
+    t1 = FakeTrioTask(name="func1")
+    assert "func1-0" == registry.get_name(t0)
+    assert "func1-1" == registry.get_name(t1)
+    assert True == registry.remove(t0)
+    with pytest.raises(RuntimeError):
+        registry.remove(t0)
--- trio_monitor/registry.py
+++ trio_monitor/registry.py
@@ -1,22 +1,84 @@
 from collections import defaultdict
+from dataclasses import dataclass
+from typing import Any, Dict, Optional, Union, cast
 
-from .protocol import TrioTask
+from .protocol import TrioNursery, TrioTask
 
 """
 Name Trio objects
 """
 
 
-class TaskRegistry:
-    def __init__(self):
-        self.serial_num_from_name = defaultdict(int)
-        self.registered_tasks = defaultdict(defaultdict)
+class SerialNumberGen:
+    """Give serial number to given names"""
 
-    def get_task_name(self, task: TrioTask):
-        if task not in self.registered_tasks:
-            serial_num = self.serial_num_from_name[task.name]
-            self.serial_num_from_name[task.name] += 1
-            self.registered_tasks[task]["serial_num"] = serial_num
+    def __init__(self):
+        self.numbers: Dict[str, int] = defaultdict(int)
+
+    def draw(self, name: str) -> int:
+        serial_num = self.numbers[name]
+        self.numbers[name] += 1
+        return serial_num
+
+
+TYPE_TRIO_TASK = 1
+TYPE_TRIO_NURSERY = 2
+TYPE_UNKOWN = -1
+
+
+def parseObjectType(object):
+    if getattr(object, "child_tasks", None) != None:
+        return TYPE_TRIO_NURSERY
+    elif getattr(object, "child_nurseries", None) != None:
+        return TYPE_TRIO_TASK
+    return TYPE_UNKOWN
+
+
+# TODO: slotted? freezed?
+@dataclass
+class RegisteredSCInfo:
+    name: str
+    serial_num: Optional[int]
+
+    @classmethod
+    def from_task(cls, task: TrioTask, serial_num: SerialNumberGen):
+        num = serial_num.draw(task.name)
+        name = f"{task.name}-{num}"
+        return cls(name=name, serial_num=num)
+
+    @classmethod
+    def from_nursery(cls, _nursery: TrioNursery, serial_num: SerialNumberGen):
+        # TODO: parse from more specific info
+        num = serial_num.draw("nursery")
+        name = f"nursery-{num}"
+        return cls(name=name, serial_num=num)
+
+
+class SCRegistry:
+    def __init__(self):
+        self.serial_drawer = SerialNumberGen()
+        self.registered: Dict[Any, RegisteredSCInfo] = {}
+
+    def get_name(self, obj: Union[TrioTask, TrioNursery]):
+        if obj in self.registered:
+            return self.registered[obj].name
+
+        objType = parseObjectType(obj)
+
+        if objType is TYPE_TRIO_TASK:
+            info = RegisteredSCInfo.from_task(cast(TrioTask, obj), self.serial_drawer)
+        elif objType is TYPE_TRIO_NURSERY:
+            info = RegisteredSCInfo.from_nursery(
+                cast(TrioNursery, obj), self.serial_drawer
+            )
         else:
-            serial_num = self.registered_tasks[task]["serial_num"]
-        return f"{task.name}-{serial_num}"
+            raise RuntimeError("[registry] Unkown type")
+        self.registered[obj] = info
+        return info.name
+
+    def remove(self, obj) -> bool:
+        if obj in self.registered:
+            del self.registered[obj]
+            return True
+        else:
+            raise RuntimeError("Bug remove item not in registry")
