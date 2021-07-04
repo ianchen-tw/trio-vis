399dbd4f9a1c3e111c367bdfb9834e9dd3956bc7
Ian Chen <ianre657@gmail.com>
2021-07-04 21:00:40 +0800

    ✅  task exit

--- tests/test_sc_monitor.py
+++ tests/test_sc_monitor.py
@@ -5,8 +5,6 @@ from trio_vis.sc_monitor import SC_Monitor
 from trio_vis.trio_fake import (FakeTrioNursery, FakeTrioTask,
                                 build_tree_from_json)
 
-# TODO: add test for node remove
-# TODO: add del task from fask_task_tree
 # TODO: refactor the test flow
 
 
@@ -23,7 +21,7 @@ class fake_logger:
 def test_root_spawned():
     logger = fake_logger()
     sc_mon = SC_Monitor(
-        cfg=VisConfig(only_vis_user_scope=False), sc_logger=Mock(return_value=logger)
+        config=VisConfig(only_vis_user_scope=False), sc_logger=Mock(return_value=logger)
     )
 
     t0 = FakeTrioTask(name="t0")
@@ -85,3 +83,96 @@ def test_normal_task_spawned_under_nursery():
     sc_mon.task_spawned(t3)
 
     logger.log_start.assert_called_once_with(child=t3, parent=n1)
+
+
+def test_task_exit():
+    start_state = {
+        "name": "t1",
+        "nurseries": [
+            {
+                "name": "n1",
+                "tasks": [
+                    {
+                        "name": "t2",
+                        "nurseries": [],
+                    },
+                    {
+                        "name": "t3",
+                        "nurseries": [],
+                    },
+                ],
+            }
+        ],
+    }
+    logger = fake_logger()
+    task_tree = build_tree_from_json(start_state)
+    sc_mon = SC_Monitor.from_tree(
+        root_task=task_tree,
+        sc_logger=Mock(return_value=logger),
+    )
+    logger.clear_cache()
+
+    t3 = task_tree.get_task_node("t3")
+    n1 = task_tree.get_nursery_node("n1")
+
+    task_tree.tree_remove("t3")
+    sc_mon.task_exited(t3)
+    logger.log_exit.assert_called_once_with(child=t3, parent=n1)
+
+
+def test_task_exit_with_nursery():
+    start_state = {
+        "name": "t1",
+        "nurseries": [
+            {
+                "name": "n1",
+                "tasks": [
+                    {
+                        "name": "t2",
+                        "nurseries": [
+                            {
+                                "name": "n2",
+                                "tasks": [],
+                            }
+                        ],
+                    },
+                ],
+            }
+        ],
+    }
+    logger = fake_logger()
+    task_tree = build_tree_from_json(start_state)
+    sc_mon = SC_Monitor.from_tree(
+        root_task=task_tree,
+        sc_logger=Mock(return_value=logger),
+    )
+    logger.clear_cache()
+
+    t2 = task_tree.get_task_node("t2")
+    n2 = task_tree.get_nursery_node("n2")
+    n1 = task_tree.get_nursery_node("n1")
+
+    task_tree.tree_remove("t2")
+    sc_mon.task_exited(t2)
+
+    required_logs = [call(child=n2, parent=t2), call(child=t2, parent=n1)]
+    logger.log_exit.assert_has_calls(required_logs)
+
+
+def test_root_task_exit():
+    start_state = {
+        "name": "t1",
+        "nurseries": [],
+    }
+    logger = fake_logger()
+    task_tree = build_tree_from_json(start_state)
+    sc_mon = SC_Monitor.from_tree(
+        root_task=task_tree,
+        sc_logger=Mock(return_value=logger),
+    )
+    logger.clear_cache()
+    t1 = task_tree.get_task_node("t1")
+    sc_mon.task_exited(t1)
+
+    logger.log_exit.assert_called_once_with(child=t1, parent=None)
+    assert sc_mon.root_exited == True
